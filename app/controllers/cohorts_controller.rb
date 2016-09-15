7a8778f6ef9cee4eb0a55e0b8dc72af336e2104c
IuliiaKot <kotlenko.julia@gmail.com>
2016-09-15 15:17:47 -0400

    display all cohorts and info about one

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,16 @@
 class CohortsController < ApplicationController
+  before_action :find_cohort, only: [:show, :edit]
+
+  def index
+    @cohorts = Cohort.all
+  end
+
+  def show
+  end
+
+  private
+
+    def find_cohort
+      @cohort = Cohort.find(params[:id])
+    end
 end
