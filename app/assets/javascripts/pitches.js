e9fd6a9cf25e9ac4005d17d3a31bedbb68f2ddf3
IuliiaKot <kotlenko.julia@gmail.com>
2016-09-22 18:11:04 -0400

    update nav bar

--- app/assets/javascripts/pitches.js
+++ app/assets/javascripts/pitches.js
@@ -1,17 +1,18 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-
+// // # Place all the behaviors and hooks related to the matching controller here.
+// // # All this logic will automatically be available in application.js.
+// // # You can use CoffeeScript in this file: http://coffeescript.org/
+//
 $(document).ready(function(){
-  $('.choose-idea').click('on', function(e){
-    debugger
+  // debugger
+  $('.choose-idea').click('on',function(e){
     var url = $(e.target).parent().find('input').attr('value');
+    debugger
     $.ajax({
       url:url,
       method: 'post'
     })
       .done(function(response){
-        // debugger
+        debugger
         if (response.count > 3) {
           var inputs = $('.choose-idea')
           for(var i = 0; i < inputs.length; i++){
