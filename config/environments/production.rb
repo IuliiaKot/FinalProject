37b3e41a73ab7617179b8aa0a87f021e707e7c7b
IuliiaKot <kotlenko.julia@gmail.com>
2016-11-01 18:29:30 -0400

    update domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,11 +98,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => "gmail.com",
+   :domain               => "dbcfinalproject.herokuapp.com",
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
-   :enable_starttls_auto => true,
-   :openssl_verify_mode  => 'none'
+   :enable_starttls_auto => true
   }
 end
