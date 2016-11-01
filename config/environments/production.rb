ac8d196d1ae6506556c3662e839650285c3db98c
IuliiaKot <kotlenko.julia@gmail.com>
2016-11-01 18:19:44 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,10 +98,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => "gmail.com",
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
-   :enable_starttls_auto => true
+   :openssl_verify_mode  => 'none'
   }
 end
