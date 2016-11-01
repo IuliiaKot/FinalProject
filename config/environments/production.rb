3ae7cf32a59dbe8fc50a5046dac4b72ad47f933c
IuliiaKot <kotlenko.julia@gmail.com>
2016-11-01 17:45:54 -0400

    add domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,9 +98,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => "mail.google.com",
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
-  :enable_starttls_auto => true
+   :enable_starttls_auto => true
   }
 end
