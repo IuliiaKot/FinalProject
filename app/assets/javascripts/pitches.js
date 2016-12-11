cffd2b1d2505514c6d0db35aca433844dbb2ff85
IuliiaKot <kotlenko.julia@gmail.com>
2016-12-10 23:09:43 -0500

    popover message for students

--- app/assets/javascripts/pitches.js
+++ app/assets/javascripts/pitches.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
       method: 'post'
     })
       .done(function(response){
+
         if (response.warning){
           $('.message-warning').prepend(
           `<div class='alert alert-warning alert-dismissible fade in' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.warning}</div>`)
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,10 +15,10 @@ class VotesController < ApplicationController
       render :json =>  {delete: "delete current vote"}
     else
       if (current_user.votes.count >= current_user.cohort.setting.student_vote_first_round)
-        render :json => {message: "You can choose only #{current_user.cohort.setting.student_vote_first_round} ideas"}
+        render :json => {warning: "You can choose only #{current_user.cohort.setting.student_vote_first_round} ideas"}
       else
         pitch.votes.create(count: 1, student_id: current_user.id)
-        render :json =>  {count: current_user.votes.count}
+        render :json =>  {count: current_user.votes.count, message: "You just vote on project #{pitch.title}"}
       end
     end
   end
