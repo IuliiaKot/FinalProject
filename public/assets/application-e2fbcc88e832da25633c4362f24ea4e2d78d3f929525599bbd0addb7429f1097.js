1307ad35fddd739f5782d122e91802598c4a5292
IuliiaKot <kotlenko.julia@gmail.com>
2016-12-11 23:40:24 -0500

    assets precompile

--- public/assets/application-e2fbcc88e832da25633c4362f24ea4e2d78d3f929525599bbd0addb7429f1097.js
+++ public/assets/application-e2fbcc88e832da25633c4362f24ea4e2d78d3f929525599bbd0addb7429f1097.js
@@ -15636,55 +15636,58 @@ var Popover = function ($) {
 
 // teacher choose projects for second round
 $(document).ready(function() {
-  $('.choose-idea').click('on',function(e){
+  $('.choose-idea').click('on', function(e) {
     var url = $(e.target).parent().find('input').attr('value');
     var element = $(e.target).parent().find('input');
     $.ajax({
-      url:url,
+      url: url,
       method: 'post'
     })
-      .done(function(response){
-        debugger
-        if (response.warning){
+      .done(function(response) {
+        if (response.warning) {
           $('.message-warning').prepend(
-          `<div class='alert alert-warning alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.warning}</div>`)
-          $(element).prop("checked", false);
-        };
-        if (response.message){
-          $('.current-votes').html(response.votes)
+          `<div class='alert alert-warning alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.warning}</div>`);
+          $(element).prop('checked', false);
+        }
+
+        if (response.message) {
+          $('.current-votes').html(response.votes);
           $('.message-warning').prepend(
-          `<div class='alert alert-success alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.message}</div>`)
-        };
-        $(".alert").delay(200).addClass("in").fadeOut(3500);
+          `<div class='alert alert-success alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.message}</div>`);
+        }
+
+        $('.alert').delay(200).addClass('in').fadeOut(3500);
         if (response.delete) {
-          $('.current-votes').html(response.votes)
-          $(element).prop("checked", false);
+          $('.current-votes').html(response.votes);
+          $(element).prop('checked', false);
         }
       });
   });
 });
-$(document).ready(function(){
-  $('.rank-project').on('change',function(e){
+$(document).ready(function() {
+  $('.rank-project').on('change',function(e) {
     var id = $(e.target).parent().attr('value');
     var url = '/pitches/' + id + '/setrank';
     var element = $(e.target);
     $.ajax({
       method: 'post',
       url: url,
-      data: {rank: $(e.target).find(':selected').text() }
+      data: { rank: $(e.target).find(':selected').text() }
     })
-      .done(function(response){
+      .done(function(response) {
         if (response) {
           if (response.warning) {
             $('.message-warning').prepend(
             `<div class='alert alert-warning alert-dismissible fade in' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.warning}</div>`);
             $(element).val('');
           }
-          if (response.message){
+
+          if (response.message) {
             $('.message-warning').prepend(
-            `<div class='alert alert-success alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.message}</div>`)
-          };
-        $(".alert").delay(200).addClass("in").fadeOut(3500);
+            `<div class='alert alert-success alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.message}</div>`);
+          }
+
+          $('.alert').delay(200).addClass('in').fadeOut(3500);
         };
 
       });
@@ -15737,27 +15740,27 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('.close').on('click', function(e){
+  $('.close').on('click', function(e) {
     e.preventDefault();
     $(this).parent().remove();
-  })
+  });
 
-  $('.rank').click('on', function(e){
+  $('.rank').click('on', function(e) {
     var url = $(e.target).parent().find('input').attr('value');
     $.ajax({
-      url:url,
+      url: url,
       method: 'post'
     })
-      .done(function(response){
+      .done(function(response) {
 
       });
   });
+
   $('.popover-dismiss').popover({
     trigger: 'focus'
   });
-
 
   let box1 = document.getElementById('team1');
   let box2 = document.getElementById('team2');
@@ -15768,14 +15771,13 @@ $(document).ready(function(){
   let allstudents = document.getElementsByClassName('student');
   dragula([box1, box2, box3, box4, box5, box6],  {
     revertOnSpill: true
-  }).on('drop', function(el){
+  }).on('drop', function(el) {
       var olParent = $(el).parent().attr('name');
       var olChield = $(el).attr('name');
-      debugger
-      if (olChield!== '' && olChield.includes(olParent)){
+      if (olChield !== '' && olChield.includes(olParent)) {
         $(el).addClass('line-pitch');
       } else {
         $(el).removeClass('line-pitch');
       }
-  });
+    });
 });
