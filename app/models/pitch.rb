a56210bcf5a87a2818edad6864544796ee1f9e4f
IuliiaKot <kotlenko.julia@gmail.com>
2016-12-14 01:23:21 -0500

    fix if statement for teams

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,7 +3,7 @@ class Pitch < ApplicationRecord
   has_many :votes
   has_many :ranks
 
-  scope :in_second_round, -> {where final: true}
+  scope :in_second_round, -> {where final: true }
 
   validates :title, presence: true
   validates :description, presence: true, length: {minimum: 10}
