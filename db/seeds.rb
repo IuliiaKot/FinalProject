e201a85387e32ed92a3aff33f5a54388507b243d
IuliiaKot <kotlenko.julia@gmail.com>
2016-12-15 12:08:39 -0500

    fix team creating issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,50 +8,50 @@
 # # Teacher.delete_all
 #
 # #
-# # Teacher.create(email: 'julia@gmail.com', password: '123456', first_name: "Iuliia", last_name: "Kotlenko")
-# Cohort.create(name: 'test')
+# Teacher.create(email: 'julia@gmail.com', password: '123456', first_name: "Iuliia", last_name: "Kotlenko")
+#
+# Cohort.create(name: 'bobolinks')
 # Setting.create(cohort_id: Cohort.last.id, pitches_per_student: 3, number_in_second_round: 4,active: true, number_of_teams: 2,student_vote_first_round: 5, active_first_round: false)
-# # students = [
-# #   {first_name: "Anna", last_name: "Kvalchuck", email: 'anna@gmail.com', password: '123456'},
-# #   {first_name: "Pan", last_name: "Wan", email: 'pan@gmail.com', password: '123456'},
-# #   {first_name: "Niel", last_name: "Geyman", email: 'niel@gmail.com', password: '123456'},
-# #   {first_name: "Elizaneth", last_name: "Layway", email: 'elizaneth@gmail.com', password: '123456'},
-# #   {first_name: "Gabriel", last_name: "Maham", email: 'gabriel@gmail.com', password: '123456'},
-# #   {first_name: "Amir", last_name: "Rafick", email: 'amir@gmail.com', password: '123456'},
-# #   {first_name: "Anthony", last_name: "Narisi", email: 'anthony@gmail.com', password: '123456'},
-# #   {first_name: "Emilia", last_name: "Friedberg", email: 'emilia@gmail.com', password: '123456'},
-# #   {first_name: "Myra", last_name: "Orgain", email: 'myra@gmail.com', password: '123456'}
-# #   # {first_name: "Neill", last_name: "Perry", email: 'neill@gmail.com', password: '123456'},
-# #   # {first_name: "Peter", last_name: "Duke", email: 'peter@gmail.com', password: '123456'},
-# #   # {first_name: "Walter", last_name: "Chabla", email: 'walter@gmail.com', password: '123456'}
-# #   ]
+# students = [
+#   {first_name: "Anna", last_name: "Kvalchuck", email: 'anna@gmail.com', password: '123456'},
+#   {first_name: "Pan", last_name: "Wan", email: 'pan@gmail.com', password: '123456'},
+#   {first_name: "Niel", last_name: "Geyman", email: 'niel@gmail.com', password: '123456'},
+#   {first_name: "Elizaneth", last_name: "Layway", email: 'elizaneth@gmail.com', password: '123456'},
+#   {first_name: "Gabriel", last_name: "Maham", email: 'gabriel@gmail.com', password: '123456'},
+#   {first_name: "Amir", last_name: "Rafick", email: 'amir@gmail.com', password: '123456'},
+#   {first_name: "Anthony", last_name: "Narisi", email: 'anthony@gmail.com', password: '123456'},
+#   {first_name: "Emilia", last_name: "Friedberg", email: 'emilia@gmail.com', password: '123456'},
+#   {first_name: "Myra", last_name: "Orgain", email: 'myra@gmail.com', password: '123456'}
+#   # {first_name: "Neill", last_name: "Perry", email: 'neill@gmail.com', password: '123456'},
+#   # {first_name: "Peter", last_name: "Duke", email: 'peter@gmail.com', password: '123456'},
+#   # {first_name: "Walter", last_name: "Chabla", email: 'walter@gmail.com', password: '123456'}
+#   ]
+#
+#   students.each do |student|
+#     Cohort.last.students.create(student)
+#   end
 # #
-# #   students.each do |student|
-# #     Cohort.last.students.create(student)
-# #   end
-# Cohort.last.students.create(first_name: "Julia", last_name: "Kotlenko", email: "kotlenko.julia@gmail.com", password: '123456')
-# #
-# #   student_pitches = Cohort.last.students
-# #   student_pitches.each do |student|
-# #     Cohort.last.setting.pitches_per_student.times do
-# #       student.pitches.create!(title: Faker::Hipster.word.capitalize, description: Faker::Hipster.paragraph(5))
-# #     end
-# #   end
+#   student_pitches = Cohort.last.students
+#   student_pitches.each do |student|
+#     Cohort.last.setting.pitches_per_student.times do
+#       student.pitches.create!(title: Faker::Hipster.word.capitalize, description: Faker::Hipster.paragraph(5))
+#     end
+#   end
 # #
 #
+
+  #
+  # Student.all.each do |student|
+  #   ids = (1..11).to_a.shuffle
+  #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
+  #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
+  #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
+  #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
+  #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
+  # end
+
+
 #
-#
-#   # Student.all.each do |student|
-#   #   ids = (1..11).to_a.shuffle
-#   #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
-#   #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
-#   #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
-#   #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
-#   #   Vote.create(pitch_id: Pitch.all[ids.shift].id, count: 1, student_id: student.id)
-#   # end
-#
-#
-# #
 
 
 # ranking
