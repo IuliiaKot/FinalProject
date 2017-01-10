4901aaed692fda3aa69a1fd510d8a2f7b7f700c2
IuliiaKot <kotlenko.julia@gmail.com>
2017-01-09 22:23:13 -0500

    add notification when vote was deleted

--- app/assets/javascripts/pitches.js
+++ app/assets/javascripts/pitches.js
@@ -26,11 +26,15 @@ $(document).ready(function() {
           `<div class='alert alert-success alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.message}</div>`);
         }
 
-        $('.alert').delay(200).addClass('in').fadeOut(3500);
+
         if (response.delete) {
           $('.current-votes').html(response.votes);
+          $('.message-warning').prepend(
+          `<div class='alert alert-danger alert-dismissible fade' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>${response.delete}</div>`);
           $(element).prop('checked', false);
         }
+
+        $('.alert').delay(200).addClass('in').fadeOut(3500);
       });
   });
 
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,14 +15,14 @@ class VotesController < ApplicationController
     if !vote_wich_exist.empty?
       vote_wich_exist.delete_all
       votes = current_user.votes.count
-      render :json =>  {delete: "delete current vote", votes: votes}
+      render :json =>  {delete: "You just deleted vote for project <strong>#{pitch.title}</strong>.", votes: votes}
     else
       if (current_user.votes.count >= current_user.cohort.setting.student_vote_first_round)
-        render :json => {warning: "You can choose only #{current_user.cohort.setting.student_vote_first_round} ideas"}
+        render :json => {warning: "You can choose only #{current_user.cohort.setting.student_vote_first_round} ideas."}
       else
         pitch.votes.create(count: 1, student_id: current_user.id)
         votes = current_user.votes.count
-        render :json =>  {count: current_user.votes.count, message: "You just vote on project #{pitch.title}", votes: votes}
+        render :json =>  {count: current_user.votes.count, message: "You just vote on project <strong>#{pitch.title}</strong>.", votes: votes}
       end
     end
   end
